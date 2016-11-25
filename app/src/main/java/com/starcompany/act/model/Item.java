5d47428c9b2cd9f9bbc984dc450f248c069cce26
yoshida0261 <yoshida261@gmail.com>
2016-11-26 03:09:00 +0900

    Initial commit

---
+++ app/src/main/java/com/starcompany/act/model/Item.java
@@ -0,0 +1,45 @@
+package com.starcompany.act.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by takeshi on 16/11/26.
+ */
+public class Item implements Parcelable {
+
+    public String id;
+    public String name;
+
+    public Item(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Item(Parcel in){
+        this.id = in.readString();
+        this.name = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeString(id);
+        dest.writeString(name);
+    }
+
+    public static final Creator<Item> CREATOR = new Creator<Item>() {
+        public Item createFromParcel(Parcel in) {
+            return new Item(in);
+        }
+
+        public Item[] newArray(int size) {
+            return new Item[size];
+        }
+    };
+}
