6866ec095efa8e7159596c916b04a7cf3ae2c7de
yoshida0261 <yoshida261@gmail.com>
2017-01-28 03:50:42 +0900

    android studio 2.2.3にupdate

--- app/src/main/java/com/starcompany/act/activity/MainActivity.java
+++ app/src/main/java/com/starcompany/act/activity/MainActivity.java
@@ -22,10 +22,12 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // 設定
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +36,7 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
--- app/src/main/java/com/starcompany/act/fragment/TaskFragment.java
+++ app/src/main/java/com/starcompany/act/fragment/TaskFragment.java
@@ -2,7 +2,12 @@ package com.starcompany.act.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.starcompany.act.model.Task;
 
@@ -11,11 +16,12 @@ import com.starcompany.act.model.Task;
  */
 public class TaskFragment extends ListFragment {
 
+    private static final String TAG = "TaskFragment";
     public TaskFragment(){}
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState)
-    {
+    public void onActivityCreated(Bundle savedInstanceState) {
+
         super.onActivityCreated(savedInstanceState);
         setListAdapter(new ArrayAdapter<String>(
                 getActivity(),
@@ -23,4 +29,21 @@ public class TaskFragment extends ListFragment {
                 Task.TaskName
         ));
     }
+
+
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+
+        Toast.makeText(v.getContext(), Task.TaskName[position], Toast.LENGTH_SHORT).show();
+
+        // タップしたitemを保存
+
+        // itemを削除
+
+
+        Log.d(TAG, "onListItemClick position => " + position + " : id => " + id);
+    }
+
+
 }
